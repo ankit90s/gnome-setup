09126dc18c07608a271a3fdadc3ae7446a46a87b
ankit90s <117509009+ankit90s@users.noreply.github.com>
2025-01-04 10:51:54 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+## check for sudo/root
+if ! [ $(id -u) = 0 ]; then
+  echo -e "\e[1;31m Please run as sudo or root \e[0m"
+  exit 1
+fi
+
+clear
+
+# Get username
+username=$(id -u -n 1000)
+builddr=$(pwd)
+
+# Copy Source file
+echo -e "\e[1;32m Copying sources.list file \e[0m"
+mv /etc/apt/sources.list /etc/apt/sources.list.bkp
+cp sources.list /etc/apt/sources.list
+apt update && apt upgrade -y
+
+# clean bloatware
+apt autoremove -y gnome-games hdate hdate-applet thunderbird totem gnome-music cheese kasumi xiterm+thai goldendict mlterm evolution
+
+# install terminal based applications
+apt install -y wget axel curl ranger htop translate-shell cmatrix alacritty exa kitty nvidia-detect fzf
+
+# install applications
+apt install -y mpv mpv-mpris
+
+# install libreoffice themes
+apt install -y libreoffice-style*
+
+# install linux mint packages
+apt install -y mint-y-icons mintstick
+
+# install restricted extras
+apt install -y ttf-mscorefonts-installer rar unrar libavcodec-extra gstreamer1.0-libav gstreamer1.0-plugins-ugly gstreamer1.0-vaapi
+
+# install microsoft compatibility layer
+apt install -y fonts-crosextra-carlito fonts-crosextra-caladea
+
+# install build-essential
+apt install -y build-essential dkms linux-headers-$(uname -r)
+
+# reboot the machine
+reboot
